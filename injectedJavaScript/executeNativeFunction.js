314e6a2d52da9217630cb783e735c04f8aae58e3
Matt Currie <me@mattcurrie.com>
2018-04-29 22:11:22 +1200

    Update executeNativeFunction.js

--- injectedJavaScript/executeNativeFunction.js
+++ injectedJavaScript/executeNativeFunction.js
@@ -1,6 +1,5 @@
 var content = `
   function executeNativeFunction(fnName, args) {
-    //window.location.hash = '&executeFunction<-' + fnName + '&' + '&arguments<-' + JSON.stringify(args) + '&';
     window.postMessage(JSON.stringify(args));
   }
 `;
